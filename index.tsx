26215b79572dbe1e0a492f98d7dd3632f489f3c5
Dzianis Papchanka <dp@citreasoft.com>
2025-11-20 17:02:45 +0300

    add grass side texture

--- index.tsx
+++ index.tsx
@@ -1,5 +1,3 @@
-
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './src/app.component';
@@ -9,5 +7,3 @@ bootstrapApplication(AppComponent, {
     provideZonelessChangeDetection()
   ]
 }).catch(err => console.error(err));
-
-// AI Studio always uses an `index.tsx` file for all project types.
